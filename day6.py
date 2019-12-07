71b74fd5acfdb470de9f55a36f6f4172e4ef8996
Dan Brosius <computer.vision.ftw@gmail.com>
2019-12-06 21:11:11 -0500

    ain't nobody got time for part 2

---
+++ day6.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+FILENAME = 'input6.txt'
+
+
+class OrbitNode:
+    def __init__(self, parent=None):
+        self.nodes = []
+        self.parent = parent
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def get_parent_orbit_count(self):
+        if self.parent == None:
+            return 0
+        else:
+            return self.parent.get_parent_orbit_count() + 1
+
+
+def main():
+    orbits = defaultdict(OrbitNode)
+    with open(FILENAME) as f:
+        for line in f:
+            parent_id, child_id = line.strip().split(")")
+            child = orbits[child_id]
+            parent = orbits[parent_id]
+            parent.add_node(child)
+            child.parent = parent
+    total_orbits = 0
+    for v in orbits.values():
+        total_orbits += v.get_parent_orbit_count()
+    return total_orbits
+
+
+if __name__ == "__main__":
+    print(main())
